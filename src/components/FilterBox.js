dacb545214e0b593656f4d06086f8aaf84d71a63
ooherin <555ohr@naver.com>
2023-06-15 14:55:56 +0900

    feat: 쿼리스트링 url 연결

--- src/components/FilterBox.js
+++ src/components/FilterBox.js
@@ -1,20 +1,42 @@
 import styled from "styled-components";
 import { Shadow } from "styles/common";
-
+import { useNavigate } from "react-router-dom";
 const FilterBox = () => {
-	// option별 onChange 함수
+	const navigate = useNavigate();
+	let url = new URL(window.location.href);
+	const params = new URLSearchParams(url.search);
+
+	//클릭한 옵션으로 콘텐츠 정렬
+	const onChangeSortOption = e => {
+		const sortValue = e.target.value;
+		//해당 key값의 params을 sortValue로 바꿈
+		params.set("sort", sortValue);
+		url.search = params.toString();
+		console.log("search", url.search);
+		navigate(`main${url.search}`);
+	};
+
+	//클릭한 옵션으로 페이지 당 콘텐츠 보여줌
+	const onChangePerPageOption = e => {
+		const perPageValue = e.target.value;
+		//해당 key값의 params을 sortValue로 바꿈
+		params.set("perPage", perPageValue);
+		url.search = params.toString();
+		navigate(`main${url.search}`);
+	};
+
 	return (
 		<S.Box>
 			<S.Wrapper>
-				<S.SelectBox>
-					<option value="업데이트순">업데이트순</option>
-					<option value="최신순">최신순</option>
-					<option value="댓글순">댓글순</option>
+				<S.SelectBox onChange={onChangeSortOption}>
+					<option value="updated">업데이트순</option>
+					<option value="current">최신순</option>
+					<option value="comments">댓글순</option>
 				</S.SelectBox>
-				<S.SelectBox>
-					<option value="10개씩">10개씩</option>
-					<option value="20개씩">20개씩</option>
-					<option value="50개씩">50개씩</option>
+				<S.SelectBox onChange={onChangePerPageOption}>
+					<option value="10">10개씩</option>
+					<option value="20">20개씩</option>
+					<option value="50">50개씩</option>
 				</S.SelectBox>
 			</S.Wrapper>
 		</S.Box>
